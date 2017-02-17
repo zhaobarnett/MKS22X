4e8b91d92874575dbfe9e7226ec8a8e56a47eb54
Barnett Zhao <zhaobarnett@gmail.com>
2017-02-16 21:29:41 -0500

    QueenBoard completed

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -3,6 +3,7 @@ public class QueenBoard{
     private int solutionCount;
 
     public QueenBoard(int size){
+	solutionCount = -1;
 	board = new int[size][size];
 	for(int row = 0; row < size; row++){
 	    for(int column = 0; column < size; column++){
@@ -21,7 +22,7 @@ public class QueenBoard{
 		board[r][column] += 1;
 	    }
 	    ///changing the diagonal values
-	    for(int x = 1; x < board[r].length; x++){
+	    for(int x = 1; x < board.length; x++){
 		if(r + x  < board.length && c + x < board.length){
 		    board[r + x][c + x] += 1;
 		}
@@ -46,13 +47,13 @@ public class QueenBoard{
 		board[r][column] -= 1;
 	    }
 	    ///changing the diagonal values
-	    for(int x = 1; x < board[r].length; x++){
+	    for(int x = 1; x < board.length; x++){
 		if(r + x  < board.length && c + x < board.length){
 		    board[r + x][c + x] -= 1;
 		}
 	    }
 	    for(int x = 1; x < board.length; x++){
-		if(r - x > 0 && c + x < board.length){
+		if(r - x >= 0 && c + x < board.length){
 		    board[r - x][c + x] -= 1;
 		}
 	    }
@@ -85,6 +86,40 @@ public class QueenBoard{
 	} return false;
     }
 
+    public void countSolutions(){
+	solutionCount = 0;
+	countSolutionsH(0);
+    }
+
+    private void countSolutionsH(int col){
+	if(col == board.length){
+	    solutionCount += 1;
+	} else{
+	for(int row = 0; row < board.length; row++){
+	    if(addQueen(row, col)){
+		countSolutionsH(col + 1);
+		removeQueen(row, col);
+	    }
+	}
+	}
+    }
+
+    public int getSolutionCount(){
+	return solutionCount;
+    }
+
+    //for testing purposes
+    public String AtoString(){
+ 	String retVal = "";
+ 	for (int r = 0; r < board.length; r++){
+ 	    for (int c = 0; c < board[r].length; c++){
+ 		retVal = retVal + board[r][c] + ' ';
+ 	    }
+ 	    retVal += "\n\n";
+ 	}
+ 	return retVal;
+    }
+
     public String toString(){
 	String retVal = "";
 	for (int r = 0; r < board.length; r++){
@@ -102,9 +137,19 @@ public class QueenBoard{
     }
 
     public static void main(String[] args){
-	QueenBoard b = new QueenBoard(2);
+	//testing if countSolution works
+	for(int x = 1; x < 11; x++){
+	    QueenBoard a = new QueenBoard(x);
+	    a.countSolutions();
+	    System.out.println(x + " by " + x + " board: " + a.getSolutionCount() + " solutions");
+	}
+	QueenBoard b = new QueenBoard(8);
+	//b.addQueen(4,2);
 	b.solve();
 	System.out.println(b);
+	//b.countSolutions();
+	//System.out.println(b.getSolutionCount());
+	//System.out.println(b);
     }
 
 }
