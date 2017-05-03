37514a183352620f7ab1d1b3f20afbce3c682185
Barnett Zhao <zhaobarnett@gmail.com>
2017-05-03 15:34:25 -0400

    started working on MyHeap in class

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,21 @@
+public class MyHeap{
+    private String[] arr;
+    private boolean max;
+
+    public MyHeap(){
+	arr = new String[10];
+	max = true;
+    }
+
+    public MyHeap(boolean b){
+	max = b;
+    }
+
+    private void resize(){
+    }
+
+    public void add(String s){
+	
+    }
+
+}
